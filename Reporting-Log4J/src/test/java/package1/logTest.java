a0b2e6ce3b1d5c003d2ba68a72d92016cce7cb12
Ali Aslantas <42274741+andrewaliscott@users.noreply.github.com>
2019-11-28 13:06:15 -0500

    Add files via upload
    Log4j and Reeporting

---
+++ Reporting-Log4J/src/test/java/package1/logTest.java
@@ -0,0 +1,33 @@
+package package1;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class logTest {
+
+	public static void main(String[] args) {
+		
+		Logger log = LogManager.getLogger(logTest.class.getName());
+		log.debug("I am logging"); 
+		log.info("Object is present"); 
+		log.debug("starting");
+
+
+
+
+
+		if(0>2) {
+			log.info("Hey");
+		}else {
+			log.error("Mey");
+		}
+	
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Reporting-Log4J/src/test/java/package2/logTest2.java
@@ -0,0 +1,33 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class logTest2 {
+
+
+	@Test
+	public void SimpleTest(){
+
+		System.out.println("Hi Everybody");
+	}
+
+	@Test
+	public void SimpleTest2(){
+
+		System.out.println("Hi Everybody");
+	}
+
+
+	@Test
+	public void SimpleTest3(){
+
+		System.out.println("Hi Everybody");
+	}
+
+	@Test
+	public void SimpleTest4(){
+
+		System.out.println("Hi Everybody");
+	}
+
+}
